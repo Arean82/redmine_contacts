343ed5ab78704e1e08b16dcbdb1b19b1e99204e6
arean82 <38061300+Arean82@users.noreply.github.com>
2025-05-28 15:16:09 +0530

    Update active_record_base_patch.rb

--- lib/redmine_contacts/patches/compatibility/active_record_base_patch.rb
+++ lib/redmine_contacts/patches/compatibility/active_record_base_patch.rb
@@ -11,7 +11,7 @@ module RedmineContacts
 
       module InstanceMethods
         def has_many_with_contacts(name, param2 = nil, *param3, &extension)
-          # If through option is used, call original has_many
+          # If options contain :through, use original has_many immediately
           if param3.is_a?(Array) && param3[0] && param3[0][:through]
             return has_many_without_contacts(name, param2, *param3, &extension)
           end
@@ -32,7 +32,12 @@ module RedmineContacts
 
           if ActiveRecord::VERSION::MAJOR >= 4
             scope, options = build_scope_and_options(options) if scope.nil?
-            has_many_without_contacts(name, scope, options, &extension)
+            if scope
+              # Pass options as keyword arguments
+              has_many_without_contacts(name, scope, **options, &extension)
+            else
+              has_many_without_contacts(name, **options, &extension)
+            end
           else
             has_many_without_contacts(name, options, &extension)
           end
@@ -41,21 +46,19 @@ module RedmineContacts
         def build_scope_and_options(options)
           scope_opts, opts = parse_options(options)
 
-          scope = if scope_opts.any?
-            lambda do
-              scope_opts.inject(self) do |result, (method, value)|
-                result.send(method, value)
-              end
+          unless scope_opts.empty?
+            scope = lambda do
+              scope_opts.inject(self) { |result, (method, arg)| result.send(method, arg) }
             end
           end
 
-          [scope, opts]
+          [defined?(scope) ? scope : nil, opts]
         end
 
         def parse_options(opts)
           scope_opts = {}
-          [:order, :having, :select, :group, :limit, :offset, :readonly].each do |key|
-            scope_opts[key] = opts.delete(key) if opts[key]
+          [:order, :having, :select, :group, :limit, :offset, :readonly].each do |o|
+            scope_opts[o] = opts.delete(o) if opts[o]
           end
           scope_opts[:where] = opts.delete(:conditions) if opts[:conditions]
           scope_opts[:joins] = opts.delete(:include) if opts[:include]
