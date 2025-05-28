7774fe84aa98bbeebe141f3632c251b6df22d297
arean82 <38061300+Arean82@users.noreply.github.com>
2025-05-28 15:23:57 +0530

    Update active_record_base_patch.rb

--- lib/redmine_contacts/patches/compatibility/active_record_base_patch.rb
+++ lib/redmine_contacts/patches/compatibility/active_record_base_patch.rb
@@ -1,58 +1,51 @@
 module RedmineContacts
   module Patches
     module ActiveRecordBasePatch
-      def self.included(base)
-        base.send(:include, InstanceMethods)
-        base.class_eval do
-          alias_method :has_many_without_contacts, :has_many
-          alias_method :has_many, :has_many_with_contacts
+      def has_many(name, *args, &extension)
+        if args.first.is_a?(Proc)
+          scope = args.shift
+        else
+          scope = nil
         end
-      end
 
-      module InstanceMethods
-        def has_many_with_contacts(name, *args, &extension)
-          if args.first.is_a?(Proc)
-            scope = args.shift
-          else
-            scope = nil
-          end
+        options = args.first || {}
 
-          options = args.first || {}
+        # Don't patch if :through option present
+        return super(name, scope, **options, &extension) if options[:through]
 
-          return has_many_without_contacts(name, scope, **options, &extension) if options[:through]
+        # Convert legacy options to modern scope calls
+        scope_opts = {}
+        scope_opts[:where]    = options.delete(:conditions) if options[:conditions]
+        scope_opts[:joins]    = options.delete(:include) if options[:include]
+        scope_opts[:distinct] = options.delete(:uniq) if options[:uniq]
 
-          scope_opts = {}
-          scope_opts[:where]     = options.delete(:conditions) if options[:conditions]
-          scope_opts[:joins]     = options.delete(:include) if options[:include]
-          scope_opts[:distinct]  = options.delete(:uniq) if options[:uniq]
+        [:order, :having, :select, :group, :limit, :offset, :readonly].each do |key|
+          scope_opts[key] = options.delete(key) if options.key?(key)
+        end
 
-          [:order, :having, :select, :group, :limit, :offset, :readonly].each do |key|
-            scope_opts[key] = options.delete(key) if options.key?(key)
-          end
-
-          if scope_opts.any?
-            scope = proc do
-              scope_opts.inject(all) do |relation, (method, arg)|
-                if arg.nil? || arg == true || arg == false
-                  relation.public_send(method)
-                else
-                  relation.public_send(method, arg)
-                end
+        if scope_opts.any?
+          scope = proc do
+            scope_opts.inject(all) do |relation, (method, arg)|
+              if arg.nil? || arg == true || arg == false
+                relation.public_send(method)
+              else
+                relation.public_send(method, arg)
               end
             end
           end
+        end
 
-          if scope
-            has_many_without_contacts(name, scope, **options, &extension)
-          else
-            has_many_without_contacts(name, **options, &extension)
-          end
+        if scope
+          super(name, scope, **options, &extension)
+        else
+          super(name, **options, &extension)
         end
       end
     end
   end
 end
 
+# Defer patching until ActiveRecord is fully loaded
 ActiveSupport.on_load(:active_record) do
-  ActiveRecord::Base.send(:include, RedmineContacts::Patches::ActiveRecordBasePatch)
+  ActiveRecord::Base.singleton_class.prepend(RedmineContacts::Patches::ActiveRecordBasePatch)
 end
