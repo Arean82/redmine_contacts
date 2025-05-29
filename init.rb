105db3ec6feb5e061e42bf8dd59a7462c65cb787
arean82 <arean82@gmail.com>
2025-05-29 12:15:38 +0530

    added CSVImportable

--- init.rb
+++ init.rb
@@ -22,6 +22,8 @@ $LOAD_PATH.unshift File.expand_path('lib', __dir__)
 requires_redmine_crm :version_or_higher => '0.0.51' rescue raise "\n\033[31mRedmine requires newer redmine_crm gem version.\nPlease update with 'bundle update redmine_crm'.\033[0m"
 
 require 'redmine'
+require_relative 'lib/csv_importable'
+
 
 CONTACTS_VERSION_NUMBER = '4.2.6'
 CONTACTS_VERSION_TYPE = "PRO version"
