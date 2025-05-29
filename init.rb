70fd41f43b950873bf8fcfb2302991e4a70f140a
arean82 <arean82@gmail.com>
2025-05-29 13:32:59 +0530

    configuring Zeitwerk inflector to recognize CSV as an acronym

--- init.rb
+++ init.rb
@@ -22,6 +22,14 @@ $LOAD_PATH.unshift File.expand_path('lib', __dir__)
 requires_redmine_crm :version_or_higher => '0.0.51' rescue raise "\n\033[31mRedmine requires newer redmine_crm gem version.\nPlease update with 'bundle update redmine_crm'.\033[0m"
 
 require 'redmine'
+
+if Rails.autoloaders.zeitwerk_enabled?
+  Rails.autoloaders.main.inflector.inflect(
+    'csv' => 'CSV'
+  )
+end
+
+
 require_relative 'lib/csv_importable'
 
 
