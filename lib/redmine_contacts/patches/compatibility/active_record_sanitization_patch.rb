ecee5fcf2adda3637874396350d99e7c445a98d3
arean82 <arean82@gmail.com>
2025-05-30 19:18:36 +0530

    realligned modules

--- lib/redmine_contacts/patches/compatibility/active_record_sanitization_patch.rb
+++ lib/redmine_contacts/patches/compatibility/active_record_sanitization_patch.rb
@@ -3,16 +3,16 @@
 module RedmineContacts
   module Patches
     module Compatibility
-    module ActiveRecordSanitizationPatch
-      def self.included(base)
-        base.class_eval do
-          def quote_value(value, column = nil)
-            connection.quote(value, column)
+      module ActiveRecordSanitizationPatch
+        def self.included(base)
+          base.class_eval do
+            def quote_value(value, column = nil)
+              connection.quote(value, column)
+            end
           end
         end
       end
     end
-  end
   end
 end
 
--- lib/redmine_contacts/patches/compatibility/user_patch.rb
+++ lib/redmine_contacts/patches/compatibility/user_patch.rb
@@ -1,35 +1,33 @@
-
-
 module RedmineContacts
   module Patches
     module Compatibility
-    module UserPatch
-      def self.included(base)
-        base.class_eval do
-          scope :having_mail, lambda {|arg|
-            addresses = Array.wrap(arg).map {|a| a.to_s.downcase}
-            if addresses.any?
-              joins(:email_addresses).where("LOWER(#{EmailAddress.table_name}.address) IN (?)", addresses).uniq
-            else
-              none
-            end
-          }
+      module UserPatch
+        def self.included(base)
+          base.class_eval do
+            scope :having_mail, lambda { |arg|
+              addresses = Array.wrap(arg).map { |a| a.to_s.downcase }
+              if addresses.any?
+                joins(:email_addresses).where("LOWER(#{EmailAddress.table_name}.address) IN (?)", addresses).uniq
+              else
+                none
+              end
+            }
 
-          def self.find_by_mail(mail)
-            if ActiveRecord::VERSION::MAJOR >= 4
-              mail.is_a?(Array) ? mail : [mail]
-              having_mail(mail).first
-            else
-              where("LOWER(mail) = ?", mail.to_s.downcase).first
+            def self.find_by_mail(mail)
+              if ActiveRecord::VERSION::MAJOR >= 4
+                mail = mail.is_a?(Array) ? mail : [mail]
+                having_mail(mail).first
+              else
+                where("LOWER(mail) = ?", mail.to_s.downcase).first
+              end
             end
           end
         end
       end
     end
   end
-  end
 end
 
-unless User.included_modules.include?(RedmineContacts::Patches::UserPatch)
-  User.send(:include, RedmineContacts::Patches::UserPatch)
+unless User.included_modules.include?(RedmineContacts::Patches::Compatibility::UserPatch)
+  User.send(:include, RedmineContacts::Patches::Compatibility::UserPatch)
 end
