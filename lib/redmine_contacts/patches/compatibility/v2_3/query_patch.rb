8270af0160aa76f3b99c02ed780d3c8dabf3b161
arean82 <arean82@gmail.com>
2025-05-30 19:31:35 +0530

    updated query patch

--- lib/redmine_contacts/patches/compatibility/v2_3/query_patch.rb
+++ lib/redmine_contacts/patches/compatibility/v2_3/query_patch.rb
@@ -2,44 +2,48 @@
 
 module RedmineContacts
   module Patches
-    module QueryPatch
-      def self.included(base) # :nodoc:
-        base.send(:include, InstanceMethods)
-        base.class_eval do
-          unloadable
-          class << self
-            VISIBILITY_PRIVATE = 0
-            VISIBILITY_ROLES   = 1
-            VISIBILITY_PUBLIC  = 2
+    module Compatibility
+      module V23
+        module QueryPatch
+          def self.included(base)
+            base.send(:include, InstanceMethods)
+            base.class_eval do
+              unloadable
+              class << self
+                VISIBILITY_PRIVATE = 0
+                VISIBILITY_ROLES   = 1
+                VISIBILITY_PUBLIC  = 2
+              end
+            end
           end
         end
-      end
-    end
 
-    module InstanceMethods
-      VISIBILITY_PRIVATE = 0
-      VISIBILITY_ROLES   = 1
-      VISIBILITY_PUBLIC  = 2
+        module InstanceMethods
+          VISIBILITY_PRIVATE = 0
+          VISIBILITY_ROLES   = 1
+          VISIBILITY_PUBLIC  = 2
 
-      def is_private?
-        visibility == VISIBILITY_PRIVATE
-      end
+          def is_private?
+            visibility == VISIBILITY_PRIVATE
+          end
 
-      def is_public?
-        !is_private?
-      end
+          def is_public?
+            !is_private?
+          end
 
-      def visibility=(value)
-        self.is_public = value == VISIBILITY_PUBLIC
-      end
+          def visibility=(value)
+            self.is_public = value == VISIBILITY_PUBLIC
+          end
 
-      def visibility
-        self.is_public ? VISIBILITY_PUBLIC : VISIBILITY_PRIVATE
+          def visibility
+            self.is_public ? VISIBILITY_PUBLIC : VISIBILITY_PRIVATE
+          end
+        end
       end
     end
   end
 end
 
-unless Query.included_modules.include?(RedmineContacts::Patches::QueryPatch)
-  Query.send(:include, RedmineContacts::Patches::QueryPatch)
+unless Query.included_modules.include?(RedmineContacts::Patches::Compatibility::V23::QueryPatch)
+  Query.send(:include, RedmineContacts::Patches::Compatibility::V23::QueryPatch)
 end
