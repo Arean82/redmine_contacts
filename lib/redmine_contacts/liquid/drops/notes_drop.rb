05855e92f064c9c17f39bf88c49843739edeec53
arean82 <arean82@gmail.com>
2025-05-30 18:30:22 +0530

    updated drop

--- lib/redmine_contacts/liquid/drops/notes_drop.rb
+++ lib/redmine_contacts/liquid/drops/notes_drop.rb
@@ -1,21 +1,6 @@
-# This file is a part of Redmine CRM (redmine_contacts) plugin,
-# customer relationship management plugin for Redmine
-#
-# Copyright (C) 2010-2019 RedmineUP
-# http://www.redmineup.com/
-#
-# redmine_contacts is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# redmine_contacts is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with redmine_contacts.  If not, see <http://www.gnu.org/licenses/>.
+module RedmineContacts
+  module Liquid
+    module Drops
 
 class NotesDrop < Liquid::Drop
 
@@ -58,4 +43,8 @@ class NoteDrop < Liquid::Drop
     @note.custom_field_values
   end
 
+end
+
+    end
+  end
 end
