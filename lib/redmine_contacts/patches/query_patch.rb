9bff9587c5ec063f10f29bec89bfea8c7bff38fe
arean82 <arean82@gmail.com>
2025-06-02 12:09:43 +0530

    reverted patches

--- lib/redmine_contacts/patches/query_patch.rb
+++ lib/redmine_contacts/patches/query_patch.rb
@@ -1,5 +1,3 @@
-
-
 require_dependency 'query'
 
 module RedmineContacts
@@ -10,18 +8,24 @@ module RedmineContacts
         base.class_eval do
           unloadable
 
-          alias_method :add_filter_without_contacts, :add_filter
-          alias_method :add_filter, :add_filter_with_contacts
+          if instance_methods.include?(:add_filter) && !method_defined?(:add_filter_with_contacts)
+            alias_method :add_filter_without_contacts, :add_filter
+            alias_method :add_filter, :add_filter_with_contacts
+          end
 
-          alias_method :add_available_filter_without_contacts, :add_available_filter
-          alias_method :add_available_filter, :add_available_filter_with_contacts
+          if instance_methods.include?(:add_available_filter) && !method_defined?(:add_available_filter_with_contacts)
+            alias_method :add_available_filter_without_contacts, :add_available_filter
+            alias_method :add_available_filter, :add_available_filter_with_contacts
+          end
         end
       end
 
       module InstanceMethods
         def add_available_filter_with_contacts(field, options)
           add_available_filter_without_contacts(field, options)
-          values = filters[field].blank? ? [] : filters[field][:values]
+          return @available_filters unless filters[field]
+
+          values = filters[field][:values] || []
           initialize_values_for_select2(field, values)
           @available_filters
         end
@@ -29,11 +33,14 @@ module RedmineContacts
         def add_filter_with_contacts(field, operator, values = nil)
           add_filter_without_contacts(field, operator, values)
           return unless available_filters[field]
+
           initialize_values_for_select2(field, values)
           true
         end
 
         def initialize_values_for_select2(field, values)
+          return unless @available_filters[field]
+
           case @available_filters[field][:type]
           when :contact, :company
             @available_filters[field][:values] = ids_to_names_with_ids(values, Contact)
@@ -43,7 +50,7 @@ module RedmineContacts
         end
 
         def ids_to_names_with_ids(ids, model)
-          ids.blank? ? [] : model.visible.where(:id => ids).map { |r| [r.name, r.id.to_s] }
+          ids.blank? ? [] : model.visible.where(id: ids).map { |r| [r.name, r.id.to_s] }
         end
       end
     end
